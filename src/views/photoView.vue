5ca2dc30c6c6beb38d29e4fb7d7ffc2f8e1f6239
zhouzihan <350636553@qq.com>
2024-03-31 02:43:00 +0800

    图片合成暂存

--- src/views/photoView.vue
+++ src/views/photoView.vue
@@ -9,55 +9,58 @@
 !-->
 
 <template>
-    <div class="container">
-        <!-- <div ref="imageTofile" class="imgContainer">
-            <img :src="currentImgUrl" alt="" class="cusimg" />
-            <div class="imginfo">
-                <div>
-                    <div>{{ currentImgInfo.Model }}</div>
-                    <div>{{ currentImgInfo.DateTime }}</div>
-                </div>
-                <div class="makeLogo">
-                    <img src="../assets/logo.png" alt="" style="width: 30px" />
-                </div>
-                <div>
-                    <div>
-                        <span>113mm f/1.6 1/170 ISO500</span>
-                    </div>
-                    <div>30°13′32″N 120°30′12″N</div>
-                </div>
-            </div>
-        </div> -->
-        <div ref="imageTofile" class="imgContainer">
-            <img :src="currentImgUrl" alt="" :style="{ width:renderOldImgXSize+'px',height:renderOldImgYSize+'px'}" />
-            <div class="imginfo" ref="imginfo" id="imginfo" :style="{ width: renderOldImgXSize + 'px' }">
-                <div>
-                    <div>{{ currentImgInfo.Model }}</div>
-                    <div>{{ currentImgInfo.DateTime }}</div>
-                </div>
-                <div class="makeLogo">
-                    <img src="../assets/logo.png" alt="" ref="makeLogo" style="width: 30px" />
-                </div>
-                <div>
-                    <div>
-                        <span>113mm f/1.6 1/170 ISO500</span>
-                    </div>
-                    <div>30°13′32″N 120°30′12″N</div>
-                </div>
-            </div>
+  <div class="container">
+    <div ref="imageTofile" class="imgContainer">
+      <img :src="currentImgUrl" alt="" class="cusimg" />
+      <div class="imginfo" v-if="currentImgInfo">
+        <div class="text-left">
+          <div>{{ currentImgInfo.Model }}</div>
+          <div>{{ currentImgInfo.DateTime }}</div>
         </div>
-
-        <div>我在测试</div>
-        <img v-show="isShow" :src="htmlUrl" alt="" />
-        <!-- <img :src="canvasImgUrl" alt=""> -->
-        <!-- <canvas ref="canvas" width="500" height="500"></canvas> -->
-
-        <van-uploader :after-read="afterRead" :before-read="beforeRead">
-            <van-button icon="plus" type="primary">上传文件</van-button>
-        </van-uploader>
-        <van-button @click="imgTofile">生成图片</van-button>
-        <van-button @click="downloadImage">生成图片</van-button>
+        <div class="makeLogo">
+          <img src="../assets/logo.png" alt="" style="width: 0.5rem" />
+        </div>
+        <div class="text-left">
+          <div>
+            <span v-if="currentImgInfo.ExposureTime"
+              >{{ currentImgInfo.FocalLengthIn35mmFilm }}mm f/{{
+                currentImgInfo.FNumber
+              }}
+              {{ reduceFraction(currentImgInfo.ExposureTime.numerator,currentImgInfo.ExposureTime.denominator) }} ISO{{
+                currentImgInfo.ISOSpeedRatings
+              }}</span
+            >
+          </div>
+          <div v-if="currentImgInfo.GPSLongitude">
+            {{
+              currentImgInfo.GPSLatitude[0] +
+              "°" +
+              currentImgInfo.GPSLatitude[1] +
+              "′" +
+              currentImgInfo.GPSLatitude[2] +
+              "″N"
+            }}
+            {{
+              currentImgInfo.GPSLongitude[0] +
+              "°" +
+              currentImgInfo.GPSLongitude[1] +
+              "′" +
+              currentImgInfo.GPSLongitude[2] +
+              "″E"
+            }}
+          </div>
+        </div>
+      </div>
     </div>
+
+    <img v-show="isShow" :src="htmlUrl" alt="" />
+
+    <van-uploader :after-read="afterRead" :before-read="beforeRead">
+      <van-button icon="plus" type="primary">上传文件</van-button>
+    </van-uploader>
+    <van-button @click="imgTofile">生成图片</van-button>
+    <!-- <van-button @click="downloadImage">生成图片</van-button> -->
+  </div>
 </template>
 
 <script>
@@ -65,202 +68,146 @@ import EXIF from "exif-js";
 import html2canvas from "html2canvas";
 
 export default {
-    name: "PhotoView",
-    data() {
-        return {
-            baseUrl: process.env.VUE_APP_BASE_API_PURCHASE,
-            currentImgUrl: "",
-            currentImgInfo: {},
-            isShow: false,
-            htmlUrl: "",
-            canvasImgUrl: "",
-            renderOldImgXSize:"",
-            renderOldImgYSize:""
-        };
-    },
-    // created() {
+  name: "PhotoView",
+  data() {
+    return {
+      baseUrl: process.env.VUE_APP_BASE_API_PURCHASE,
+      currentImgUrl: "",
+      currentImgInfo: null,
+      isShow: false,
+      htmlUrl: "",
+      canvasImgUrl: "",
+      renderOldImgXSize: "",
+      renderOldImgYSize: "",
+    };
+  },
+  // created() {
 
+  // },
+  mounted() {},
+  methods: {
+    //上传前
+    beforeRead(file) {
+      alert(file.type);
+      if (file.type !== "image/jpeg") {
+        Toast("请上传 jpg 格式图片");
+        return false;
+      }
+      return true;
+    },
+    //上传后
+    afterRead(file) {
+      console.log("file", file);
+      this.currentImgUrl = file.content;
+      let that = this;
+      EXIF.getData(file.file, function () {
+        console.log("getData", this);
+        // 这里面可以看到值，想要什么直接获取即可。
+        let currentImgInfo = EXIF.getAllTags(this);
+        console.log("currentImgInfo", currentImgInfo);
+        that.currentImgInfo = currentImgInfo;
+      });
+    },
+    imgTofile() {
+      let renderDom = this.$refs.imageTofile;
+      let width = renderDom.offsetWidth;
+      let height = renderDom.offsetHeight;
+
+      let max = Math.max(width, height);
+      let scale = parseInt(4000 / max);
+
+      console.log('scale',scale)
+
+      html2canvas(this.$refs.imageTofile, {
+        backgroundColor: "#fff",
+        scale: scale,
+        // width: width*scale,
+        // height:height*scale
+      })
+        .then((canvas) => {
+          let url = canvas.toDataURL("image/png");
+          this.htmlUrl = url;
+          this.$nextTick(() => {
+            this.isShow = true;
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    base64ToFile: function (urlData, fileName) {
+      let arr = urlData.split(",");
+      let mime = arr[0].match(/:(.*?);/)[1];
+      let bytes = atob(arr[1]);
+      let n = bytes.length;
+      let ia = new Uint8Array(n);
+      while (n--) {
+        ia[n] = bytes.charCodeAt(n);
+      }
+      return new File([ia], fileName, { type: mime });
+    },
+    drawImage(imgUrl) {
+      const canvas = this.$refs.canvas;
+      const ctx = canvas.getContext("2d");
+      const image = new Image();
+
+      image.onload = () => {
+        canvas.width = image.width;
+        canvas.height = image.height;
+        // 绘制图片
+        ctx.drawImage(image, 0, 0);
+
+        ctx.fillStyle = "red";
+        ctx.fillRect(10, 10, 50, 50);
+      };
+
+      image.src = imgUrl; // 替换为你的图片路径
+    },
+    //找到分子分母的最大公约数
+    findGCD(num1, num2) {
+      while (num2 !== 0) {
+        var temp = num2;
+        num2 = num1 % num2;
+        num1 = temp;
+      }
+      return num1;
+    },
+    //分子分母到最简形式 numerator分子 denominator分母
+    reduceFraction(numerator, denominator) {
+      var gcd = this.findGCD(numerator, denominator);
+      return numerator / gcd + "/" + denominator / gcd;
+    },
+    // downloadImage() {
+    //   const canvas = this.$refs.canvas;
+    //   const dataURL = canvas.toDataURL("image/jpeg", 1.0);
+
+    //   const link = document.createElement("a");
+    //   link.href = dataURL;
+    //   this.canvasImgUrl = dataURL;
+    //   link.download = "my-image.jpeg";
+    //   link.click();
     // },
-    mounted() { },
-    methods: {
-        //上传前
-        beforeRead(file) {
-            if (file.type !== "image/jpeg") {
-                Toast("请上传 jpg 格式图片");
-                return false;
-            }
-            return true;
-        },
-        //上传后
-        afterRead(file) {
-            console.log("file", file);
-            this.currentImgUrl = file.content;
-            let that = this;
-            EXIF.getData(file.file, function () {
-                console.log("getData", this);
-                // 这里面可以看到值，想要什么直接获取即可。
-                // console.log('getAllTags', EXIF.getAllTags(this));
-                let currentImgInfo = EXIF.getAllTags(this);
-                console.log("currentImgInfo", currentImgInfo);
-                that.currentImgInfo = currentImgInfo;
-
-                let maxSize = Math.max(that.currentImgInfo.PixelXDimension, that.currentImgInfo.PixelYDimension);
-                // let renderOldImgXSize;
-                // let renderOldImgYSize;
-
-                // 获取目标DOM元素
-                var element = document.getElementById('imginfo');
-
-                // 获取生成图片的尺寸
-                // var width = this.currentImgInfo.PixelXDimension;
-                // var height = this.currentImgInfo.PixelYDimension;
-                var width=this.renderOldImgXSize;
-                var height=this.renderOldImgYSize;
-
-                // 计算动态字体大小
-                var fontSize = Math.min(width, height) * 0.05; // 例如，根据尺寸比例设置字体大小
-                console.log('font-size', fontSize)
-                // 设置动态字体大小
-                element.style.fontSize = fontSize + 'px';
-                console.log(that.$refs.imginfo.offsetHeight);
-                that.$refs.makeLogo.style.width = fontSize + 'px'
-
-
-
-                if (maxSize == that.currentImgInfo.PixelXDimension) {
-                    if (maxSize > 4000) {
-                        that.renderOldImgXSize = 4000;
-                        // that.currentImgInfo.PixelXDimension/that.currentImgInfo.PixelYDimension==4000/X
-                        that.renderOldImgYSize = 4000 / (that.currentImgInfo.PixelXDimension / that.currentImgInfo.PixelYDimension);
-                    }
-                }
-                else{
-                    if(maxSize>4000){
-                        that.renderOldImgYSize = 4000;
-                        that.renderOldImgXSize=4000 / (that.currentImgInfo.PixelYDimension / that.currentImgInfo.PixelXDimension);
-                    }
-                }
-
-
-                // that.drawImage(file.content);
-            });
-        },
-        imgTofile() {
-            // let width = "";
-            // let height = "";
-
-            
-
-            // let maxSize = Math.max(this.currentImgInfo.PixelXDimension, this.currentImgInfo.PixelYDimension);
-            // let renderXSize;
-            // let renderYSize;
-            // if (maxSize == this.currentImgInfo.PixelXDimension) {
-            //     if (maxSize > 4000) {
-            //         renderXSize = 4000;
-            //         // this.currentImgInfo.PixelXDimension/this.currentImgInfo.PixelYDimension==4000/X
-            //         renderYSize = 4000 / (this.currentImgInfo.PixelXDimension / this.currentImgInfo.PixelYDimension);
-            //         console.log("进来了", renderYSize)
-            //     }
-
-            // }
-            // else {
-            //     renderXSize = this.currentImgInfo.PixelXDimension;
-            //     renderYSize = this.currentImgInfo.PixelYDimension;
-
-            // }
-
-
-
-            // console.log(this.currentImgInfo.PixelXDimension, this.currentImgInfo.PixelYDimension)
-            // console.log(renderXSize, renderYSize)
-            html2canvas(this.$refs.imageTofile, {
-                // backgroundColor: "red",
-                // width: renderXSize,
-                width:this.renderOldImgXSize,
-                // height: this.currentImgInfo.PixelYDimension + this.$refs.imginfo.offsetHeight,
-                height: this.renderOldImgYSize+this.$refs.imginfo.offsetHeight,
-                // height: renderYSize,
-                scale: 1,
-            }).then((canvas) => {
-                console.log(canvas)
-                let url = canvas.toDataURL("image/png");
-                this.htmlUrl = url;
-                this.$nextTick(() => {
-                    this.isShow = true;
-                })
-                // const link = document.createElement("a");
-                // link.href = url;
-                // link.download = "my-image.jpeg";
-                // link.click();
-                
-
-            }).catch((err) => {
-                console.log(err);
-                alert(err)
-            });
-        },
-        base64ToFile: function (urlData, fileName) {
-            let arr = urlData.split(",");
-            let mime = arr[0].match(/:(.*?);/)[1];
-            let bytes = atob(arr[1]);
-            let n = bytes.length;
-            let ia = new Uint8Array(n);
-            while (n--) {
-                ia[n] = bytes.charCodeAt(n);
-            }
-            return new File([ia], fileName, { type: mime });
-        },
-        drawImage(imgUrl) {
-            const canvas = this.$refs.canvas;
-            const ctx = canvas.getContext("2d");
-            const image = new Image();
-
-            image.onload = () => {
-                canvas.width = image.width;
-                canvas.height = image.height;
-                // 绘制图片
-                ctx.drawImage(image, 0, 0);
-
-                ctx.fillStyle = "red";
-                ctx.fillRect(10, 10, 50, 50);
-
-                // ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-            };
-
-            image.src = imgUrl; // 替换为你的图片路径
-        },
-        downloadImage() {
-            const canvas = this.$refs.canvas;
-            const dataURL = canvas.toDataURL("image/jpeg", 1.0);
-
-            const link = document.createElement("a");
-            link.href = dataURL;
-            this.canvasImgUrl = dataURL;
-            link.download = "my-image.jpeg";
-            link.click();
-        },
-    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .container {
-    width: 80%;
-    margin: 0 auto;
 
-    >div.imgContainer {
-        .cusimg {
-            width: 100%;
-        }
-
-        .imginfo {
-            width: 100%;
-            padding: 5px;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            // font-size: 0.4rem;
-        }
+  > div.imgContainer {
+    .cusimg {
+      width: 100%;
     }
+
+    .imginfo {
+      width: 100%;
+      padding: 5px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      font-size: 0.26rem;
+      box-sizing: border-box;
+    }
+  }
 }
 </style>
